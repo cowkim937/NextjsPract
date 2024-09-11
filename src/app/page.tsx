e1ba2b2250057acd32949deee95340b33b34f6a7
cowkim937 <77529767+cowkim937@users.noreply.github.com>
2024-09-11 19:57:43 +0900

    First Upload

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <p className="text-7xl font-serif font-bold text-sky-700">Hello, World!</p>
+        <p className="text-2xl text-justify text-center">DDDDDD<br/>AAAAAA</p>
+
+        <Link href={"/Info"}>About</Link>
+      </main>
+    </div>
+  );
+}
